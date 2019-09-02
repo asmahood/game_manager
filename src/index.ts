c1d08402f434bf672f6dfd645f45b33d478f9e80
Adam Mahood <mahood.adam@gmail.com>
2019-09-02 16:14:47 -0400

    [src] move exit into then block

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { client as db } from './db';
 
 db.query('SELECT NOW()')
-  .then((res): void => console.log(res))
-  .catch((err): void => console.error(err));
-
-process.exit(0);
+  .then((res): void => {
+    console.log(res);
+    process.exit(0);
+  })
+  .catch((err): void => {
+    console.error(err);
+    process.exit(1);
+  });
 
 // process.stdin.on('readable', (): void => {
 //   let chunk = process.stdin.read();
