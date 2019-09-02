74f086192bce33186bb0f1c7955c53287175e313
Adam Mahood <mahood.adam@gmail.com>
2019-09-02 14:47:13 -0400

    [style] fix string quote style

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,5 @@
 import { Client } from 'pg';
 
-// DB configuration variables
-// import { db } from '../config.json';
-
 export const client = new Client({
   host: process.env.DB_HOST as string,
   database: process.env.DB_DATABASE as string,
@@ -13,5 +10,5 @@ export const client = new Client({
 
 // connect client to the server
 client.connect()
-  .then((): void => console.log("client connected to server"))
+  .then((): void => console.log('client connected to server'))
   .catch((err): void => console.error(`error connecting to server: ${err}`));
