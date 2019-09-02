0098dd35777d4c18769de5b2b8eb0f65c4a61101
Adam Mahood <mahood.adam@gmail.com>
2019-09-02 17:16:29 -0400

    [build] Add Travis-CI for automated builds (#1)
    * [build] add travis config file
    
    * [src] use env variables instead of json file
    
    * [build] fix script and use npm build script
    
    * [build] add pg service to travis build
    
    * [deps] add dotenv to dev deps
    
    * [style] fix string quote style
    
    * [build] add before script command for pg
    
    * [build] fix typo in service
    
    * [src] add exit so build finishes
    
    * [style] add new line after error msg
    
    * [src] move exit into then block

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,14 @@
 import { Client } from 'pg';
 
-// DB configuration variables
-import { db } from '../config.json';
-
 export const client = new Client({
-  host: db.HOST,
-  database: db.DATABASE,
-  user: db.USER,
-  password: db.PASSWORD,
-  port: db.PORT
+  host: process.env.DB_HOST as string,
+  database: process.env.DB_DATABASE as string,
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string,
+  port: parseInt(process.env.DB_PORT as string)
 });
 
 // connect client to the server
 client.connect()
-  .then((): void => console.log("client connected to server"))
-  .catch((err): void => console.error(`error connecting to server: ${err}`));
+  .then((): void => console.log('client connected to server'))
+  .catch((err): void => console.error(`error connecting to server: \n${err}`));
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { client as db } from './db';
 
 db.query('SELECT NOW()')
-  .then((res): void => console.log(res))
-  .catch((err): void => console.error(err));
+  .then((res): void => {
+    console.log(res);
+    process.exit(0);
+  })
+  .catch((err): void => {
+    console.error(err);
+    process.exit(1);
+  });
 
 // process.stdin.on('readable', (): void => {
 //   let chunk = process.stdin.read();
