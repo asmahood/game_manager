e80ff345a5c1b980b09eea9431a27007bbbf13c9
Adam Mahood <mahood.adam@gmail.com>
2019-12-26 00:05:20 -0500

    [src] implemented print_referees function to list referees

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import Referee from './Referee'
 
 import { referees } from '../mock_data.json';
 
+import process from 'process';
+
 function init_referees(): Referee[] {
   let refs: Referee[] = [];
 
   for (const r of referees) {
-    const ref = new Referee(r.id, r.first_name, r.last_name);
+    const ref = new Referee(r.id, r.first_name, r.last_name, r.level);
 
     refs.push(ref);
   }
@@ -14,6 +16,16 @@ function init_referees(): Referee[] {
   return refs;
 }
 
+function print_referees(refs: Referee[]): void {
+  let s: string = '';
+  
+  for (const r of refs) {
+    s += `Referee #${r.getID()}\nName: ${r.getFullName()}\nLevel: ${r.getLevel()}\n`
+  }
+
+  process.stdout.write(s);
+}
+
 const refs = init_referees();
 
-console.log(refs);
+print_referees(refs);
